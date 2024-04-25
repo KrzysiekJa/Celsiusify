85bd958edb0be8278fa1efd2cbf948fb16225432
KrzysiekJa <kjmj77@gmail.com>
2024-04-25 22:54:19 +0200

    Moved back to python version 3.9.

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 import random
 import string
-from typing import Dict, Union
+from typing import Dict
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> None:
+app = FastAPI(title='Celsiusify')
+
+@app.on_event("startup")
+async def startup_event() -> None:
     string_len, num_of_strs = 8, 4
     list_of_ids = [''.join(random.choices(string.ascii_letters + string.digits, k=string_len)) for _ in range(num_of_strs)]
     app.app_identifier = '-'.join(list_of_ids)
 
-app = FastAPI(title='Celsiusify', lifespan=lifespan)
-
 @app.get("/convert/")
-async def convert_temperature(fahrenheit: float) -> Dict[str, Union[int, str]]:
+async def convert_temperature(fahrenheit: float) -> Dict[str, str]:
     # temperature in degrees Celsius (°C) = (temperature in degrees Fahrenheit (°F) - 32) * 5/9
     celsius = (fahrenheit - 32) * 5.0/9.0
-    return {"celsius": celsius, "app_identifier": app.app_identifier}
+    return {"celsius": f'{celsius:.6f}', "app_identifier": app.app_identifier}
