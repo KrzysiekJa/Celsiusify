00dc0eed0ecede5ffa4a644a6839c9e8f9cba42c
KrzysiekJa <kjmj77@gmail.com>
2024-05-04 21:13:16 +0200

    Fixed error ci.yml; added docstrings in app/main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+"""
+Main module for the Celsiusify application.
+"""
 import random
 import string
 from typing import Dict
@@ -8,12 +11,25 @@ app = FastAPI(title='Celsiusify')
 
 @app.on_event("startup")
 async def startup_event() -> None:
-    string_len = 8
-    list_of_ids = [''.join(random.choices(string.ascii_letters + string.digits, k=string_len)) for _ in range(stop=4)]
+    """
+    Generate and set a random application identifier on startup.
+    """
+    string_len, constants_str = 8, string.ascii_letters + string.digits
+    list_of_ids = [''.join(random.choices(constants_str, k=string_len)) for _ in range(stop=4)]
     app.app_identifier = '-'.join(list_of_ids)
 
 @app.get("/convert/")
 async def convert_temperature(fahrenheit: float) -> Dict[str, str]:
-    # temperature in degrees Celsius (°C) = (temperature in degrees Fahrenheit (°F) - 32) * 5/9
+    """
+    Convert temperature from Fahrenheit to Celsius, following the equation:
+    
+    temperature in degrees Celsius (°C) = (temperature in degrees Fahrenheit (°F) - 32) * 5/9
+    
+    Args:
+        fahrenheit (float): Temperature in Fahrenheit.
+
+    Returns:
+        Dict[str, str]: Dictionary containing converted temperature in Celsius and application identifier.
+    """   
     celsius = (fahrenheit - 32) * 5.0/9.0
     return {"celsius": f'{celsius:.6f}', "app_identifier": app.app_identifier}
